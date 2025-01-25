9404dca13d19c096a1d40398c087068728c5e0ee
Tori-Kinia <tori.kinia@gmail.com>
2025-01-25 11:05:08 +0100

    Update TabBarSettingsController.m

--- Source/Prefs/TabBarSettingsController.m
+++ Source/Prefs/TabBarSettingsController.m
@@ -108,10 +108,9 @@
 
 - (UIImage *)tbImageNamed:(NSString *)imageName {
     BOOL isDownloads = [imageName isEqualToString:@"icons/downloads"];
-    imageName.WTintColor = [UIColor colorWithRed:30.0/255.0 green:150.0/255.0 blue:245.0/255.0 alpha:1.0];
     YTAssetLoader *al = [[NSClassFromString(@"YTAssetLoader") alloc] initWithBundle:isDownloads ? NSBundle.ytmu_defaultBundle : [NSBundle mainBundle]];
 
-    return [al imageNamed:imageName color:WTintColor];
+    return [al imageNamed:imageName];
 }
 
 - (BOOL)tableView:(UITableView *)tableView shouldHighlightRowAtIndexPath:(NSIndexPath *)indexPath {
