0f4881aa9673463704ab5807f448fefea511de14
Tori-Kinia <tori.kinia@gmail.com>
2025-01-25 11:37:38 +0100

    Update YTAssetLoader.h

--- Source/Headers/YTAssetLoader.h
+++ Source/Headers/YTAssetLoader.h
@@ -3,5 +3,4 @@
 @interface YTAssetLoader : NSObject
 - (instancetype)initWithBundle:(NSBundle *)bundle;
 - (UIImage *)imageNamed:(NSString *)image;
-- (UIImage *)tintImage:(UIImage *)image color:(UIColor *)color;
 @end
