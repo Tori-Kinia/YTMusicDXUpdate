3f76c1e6ce242b9a93a60f7e9381822612782728
Tori-Kinia <tori.kinia@gmail.com>
2025-01-25 12:28:52 +0100

    Update TabBarSettingsController.m

--- Source/Prefs/TabBarSettingsController.m
+++ Source/Prefs/TabBarSettingsController.m
@@ -107,7 +107,7 @@
 }
 
 - (UIImage *)tbImageNamed:(NSString *)imageName {
-    BOOL isDownloads = [imageName isEqualToString:@"downloads"];
+    BOOL isDownloads = [imageName isEqualToString:@"icons/downloads"];
     YTAssetLoader *al = [[NSClassFromString(@"YTAssetLoader") alloc] initWithBundle:isDownloads ? NSBundle.ytmu_defaultBundle : [NSBundle mainBundle]];
 
     return [al imageNamed:imageName];
